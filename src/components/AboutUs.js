a3064f7c95dc012f18d9fc00f6ca14d2e1392262
Rica Mae Averion <ricamaeaverion22@gmail.com>
2022-06-23 10:56:15 +1000

    Added buttons that redirect to different paths

--- src/components/AboutUs.js
+++ src/components/AboutUs.js
@@ -1,7 +1,21 @@
 import React from "react";
+import { useNavigate } from "react-router-dom";
 
 function AboutUs() {
-    return <div>About us</div>;
+
+    // Function that navigates to a different route
+    let navigate = useNavigate();
+
+    return (
+        <div>
+            <div>
+                About us
+            </div>
+            <div>
+                <button onClick={ () => navigate("/becomeatutor") }>WANT TO JOIN OUR TEAM?</button>
+            </div>
+        </div>
+    );
 }
 
 export default AboutUs;
--- src/components/BecomeATutor.js
+++ src/components/BecomeATutor.js
@@ -1,7 +1,21 @@
 import React from "react";
+import { useNavigate } from "react-router-dom";
 
 function BecomeATutor() {
-    return <div>Become a tutor</div>;
+
+    // Function that navigates to a different route
+    let navigate = useNavigate();
+
+    return (
+        <div>
+            <div>
+                Become a tutor
+            </div>
+            <div>
+                <button onClick={ () => navigate("/contactus") }>JOIN UR TEAM</button>
+            </div>
+        </div>
+    );
 }
 
 export default BecomeATutor;
