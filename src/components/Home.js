781df221e80d009f86e7c8d9e616882623ab2890
Rica Mae Averion <ricamaeaverion22@gmail.com>
2022-06-23 10:56:15 +1000

    Added buttons that redirect to different paths

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,7 +1,21 @@
 import React from "react";
+import { useNavigate } from "react-router-dom";
 
 function Home() {
-    return <div>Homepage</div>;
+
+    // Function that navigates to a different route
+    let navigate = useNavigate();
+
+    return (
+        <div>
+            <div>
+                Homepage
+            </div>
+            <div>
+                <button onClick={ () => navigate("/contactus") }>CLICK HERE TO FIND A TUTOR</button>
+            </div>
+        </div>
+    );
 }
 
 export default Home;
