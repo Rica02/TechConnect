fa20b9c744c4dfb0336ecbd877cef80fe4335d88
Rica Mae Averion <ricamaeaverion22@gmail.com>
2022-07-05 18:27:12 +1000

    added tutors images

--- src/styles/PageComponents.style.js
+++ src/styles/PageComponents.style.js
@@ -81,23 +81,25 @@ export const StyledContainer = styled.div`
     .grid-container-what-we-do{
         display: grid;
         grid-template-columns: auto auto;
-        //background-color: #2196F3;
         padding: 10px;
     }
 
     .grid-item {
         display: flex;
         align-items: center;
-        //background-color: rgba(255, 255, 255, 0.8);
-        //border: 1px solid rgba(0, 0, 0, 0.8);
         padding: 20px;
         font-size: 30px;
+
+        img {
+            height: 80px;
+            margin: 20px;
+            border: solid black 1px
+        }
     }
 
     .grid-container-tutors{
         display: grid;
         grid-template-columns: auto auto auto;
-        //background-color: #2196F3;
         padding: 10px;
     }
 `
