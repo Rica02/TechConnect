5223777fc7b989ad7680407dfbd6c5085838fb7c
Bo-HAN <leolucky90@gmail.com>
2022-07-08 01:45:59 +1000

    Revert "Added main components of News page"
    This reverts commit 5e8966ee7fd6b4cea50cb86bac547b4926972a24.

--- src/pages/News/index.js
+++ src/pages/News/index.js
@@ -1,14 +1,11 @@
 import React from "react";
 import Header from "../Header";
 import headerBanner from "../../assets/images/news-banner.jpg";
-import NewsItem from "./NewsItem";
 
 function News() {
     return (
         <>
             <Header headerTitle={"News"} headerBanner={headerBanner} />
-            {/* TODO: make news items dynamic */}
-            <NewsItem />
         </>
     );
 }
