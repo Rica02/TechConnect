f5f911b38146666fa0817ab7a8f0120f60eb33de
Rica Mae Averion <ricamaeaverion22@gmail.com>
2022-07-07 15:43:45 +1000

    Added Contact Us page components

---
+++ src/pages/ContactUs/components/FindOurOffice/index.js
@@ -0,0 +1,12 @@
+import React from "react";
+import { Container } from "./FindOurOfficeStyle";
+//import { Wrapper, Status } from "@googlemaps/react-wrapper";
+
+export default function FindOurOffice(){
+    return(
+        <Container>
+            <h2>Find our office</h2>
+            <p># Street Address, City, STATE, 0000</p>
+        </Container>
+    )
+}
