69b04b22621d3ab07e520e4e863eb6b543470663
Rica Mae Averion <ricamaeaverion22@gmail.com>
2022-07-07 17:25:22 +1000

    Added Google Maps

--- src/pages/ContactUs/components/FindOurOffice/index.js
+++ src/pages/ContactUs/components/FindOurOffice/index.js
@@ -1,12 +1,14 @@
 import React from "react";
+import GoogleMaps from "../GoogleMaps";
 import { Container } from "./FindOurOfficeStyle";
-//import { Wrapper, Status } from "@googlemaps/react-wrapper";
 
 export default function FindOurOffice(){
+
     return(
         <Container>
             <h2>Find our office</h2>
             <p># Street Address, City, STATE, 0000</p>
+            <GoogleMaps />
         </Container>
     )
 }
