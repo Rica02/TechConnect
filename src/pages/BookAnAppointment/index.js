b0b438f0ab6f0b59f63f0179b3f4639c3b3b6e85
Rica Mae Averion <ricamaeaverion22@gmail.com>
2022-07-07 21:40:36 +1000

    Added Book An Appointment components

--- src/pages/BookAnAppointment/index.js
+++ src/pages/BookAnAppointment/index.js
@@ -1,11 +1,13 @@
 import React from "react";
 import Header from "../Header";
 import headerBanner from "../../assets/images/book-an-appointment-banner.jpg";
+import Form from "./components/Form";
 
 function BookAnAppointment() {
     return (
         <>
             <Header headerTitle={"Book an appointment"} headerBanner={headerBanner} />
+            <Form />
         </>
     );
 }
