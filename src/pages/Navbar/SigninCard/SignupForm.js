31b2b1cb848e8a5f8c39b220877b6bcdf14f9ba5
Bo-HAN <leolucky90@gmail.com>
2022-07-28 17:21:55 +1000

    sign Up page update

--- src/pages/Navbar/SigninCard/SignupForm.js
+++ src/pages/Navbar/SigninCard/SignupForm.js
@@ -9,7 +9,8 @@ import {
 function SignupForm() {
   const [formInput, setFormInput] = useState({
     email: "",
-    password: ""
+    password: "",
+    passwordConfirm:"",
   })
 
   var handleForm = (e) => {
@@ -17,7 +18,11 @@ function SignupForm() {
     setFormInput({ ...formInput, [e.target.name]: value })
     console.log(formInput);
   }
+
   var regFunction = async () => {
+    if(formInput.password!==formInput.passwordConfirm){
+      return  alert("password Confirm Different")
+    }
     console.log(formInput.email + " " + formInput.password);
     debugger
     await axios.post("http://localhost:3007/register", {
@@ -27,7 +32,7 @@ function SignupForm() {
       if (result) {
         console.log(result);
         alert("succeeded register")
-        debugger
+       
       } else {
         console.log(reject)
         alert(reject)
@@ -42,7 +47,7 @@ function SignupForm() {
       <FormLabel htmlFor='for'>Password</FormLabel>
       <FormInput onChange={handleForm} name="password" type='password' required />
       <FormLabel htmlFor='for'>Password confirm</FormLabel>
-      <FormInput type='password' required />
+      <FormInput type='password' required name="passwordConfirm" onChange={handleForm}/>
       <FormButton type='submitSignUp' onClick={() => regFunction()}>continue</FormButton>
     </>
   )
--- src/pages/Signin/Signup.js
+++ src/pages/Signin/Signup.js
@@ -1,4 +1,5 @@
-import React from "react";
+import React, { useState } from "react";
+import axios from "axios";
 import { useNavigate } from "react-router-dom";
 import {
     Container,
@@ -8,36 +9,66 @@ import {
     FormInput,
     FormWrap,
     Icon,
-
-    Form ,
+    Form,
     FormLabel
 } from './SigninStyle';
 
 function Signup() {
     // Function that navigates to a different route
     let navigate = useNavigate();
-    
+    const [formInput, setFormInput] = useState({
+        email: "",
+        password: "",
+        passwordConfirm: "",
+    })
+
+    var handleForm = (e) => {
+        var value = e.target.value
+        setFormInput({ ...formInput, [e.target.name]: value })
+        console.log(formInput);
+    }
+
+    var regFunction = async () => {
+        if (formInput.password !== formInput.passwordConfirm) {
+            return alert("password Confirm Different")
+        }
+        console.log(formInput.email + " " + formInput.password);
+        debugger
+        await axios.post("http://localhost:3007/register", {
+            email: formInput.email,
+            password: formInput.password
+        }).then((result, reject) => {
+            if (result) {
+                console.log(result);
+                alert("succeeded register")
+
+            } else {
+                console.log(reject)
+                alert(reject)
+            }
+        })
+    }
     return (
         <>
-        <Container>
-            <FormWrap>
-                <Icon to="/"></Icon>
-                <FormContent>
-                    <Form action="#">
-                        <FormH1>Sign in to your account</FormH1>
-                        <FormLabel htmlFor='for'>Email</FormLabel>
-                        <FormInput type='email' required/>
-                        <FormLabel htmlFor='for'>Password</FormLabel>
-                        <FormInput type='password' required/>
-                        <FormLabel htmlFor='for'>Password</FormLabel>
-                        <FormInput type='password' required/>
-                        <FormButton type='submitSignup'>SIGN UP</FormButton>
-                        <FormButton onClick={ () => navigate("/signin") }>GO SIGN IN</FormButton>
-                    </Form>
+            <Container>
+                <FormWrap>
+                    <Icon to="/"></Icon>
+                    <FormContent>
+                        <Form action="#">
+                            <FormH1>Creat your account</FormH1>
+                            <FormLabel htmlFor='for'>Email</FormLabel>
+                            <FormInput onChange={handleForm} name="email" type='email' required />
+                            <FormLabel htmlFor='for'>Password</FormLabel>
+                            <FormInput onChange={handleForm} name="password" type='password' required />
+                            <FormLabel htmlFor='for'>Password confirm</FormLabel>
+                            <FormInput type='password' required  name="passwordConfirm" onChange={handleForm}/>
+                            <FormButton type='submitSignUp' onClick={() => regFunction()}>continue</FormButton>
+                            <FormButton onClick={() => navigate("/signin")}>GO SIGN IN</FormButton>
+                        </Form>
 
-                </FormContent>
-            </FormWrap>
-        </Container>
+                    </FormContent>
+                </FormWrap>
+            </Container>
         </>
     );
 }
