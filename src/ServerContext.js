6b0b5fba3f2f85c353b19f84120fdedade9505fa
Matthew Nguyen <matthew.ng.id20@gmail.com>
2022-08-05 14:38:23 +1000

    database changes and encrypt and decrypt

--- src/ServerContext.js
+++ src/ServerContext.js
@@ -2,52 +2,52 @@ import React, { useContext, useState, useEffect } from "react"
 const ServerContext = React.createContext()
 
 export function useDB() {
-    return useContext(ServerContext)
+  return useContext(ServerContext)
+}
+
+export function ServerProvider({ children }) {
+  const [auth, setAuth] = useState({});
+  const [currentUser, setCurrentUser] = useState({})
+  const [email, setEmail] = useState()
+  const [admin, setadmin] = useState()
+  const [loginState, setloginState] = useState(false)
+  function SetloginState(bool) {
+    return setloginState(bool)
+  }
+  function userSet(email, admin) {
+    localStorage.setItem("email", email)
+    localStorage.setItem("admin", admin)
+  }
+  function logout() {
+    setCurrentUser(null)
+    localStorage.clear()
+    window.location.reload()
+  }
+  function userget() {
+    setEmail(localStorage.getItem("email"))
+    setadmin(localStorage.getItem("admin"))
   }
 
-  export function ServerProvider({ children }) {
-    const [auth, setAuth] = useState({});
-    const [currentUser, setCurrentUser] = useState({})
-    const [email,setEmail]=useState()
-    const [admin,setadmin]=useState()
-    const[loginState,setloginState]=useState(false)
-    function SetloginState(bool){
-      return setloginState(bool)
-    }
-    function userSet(email,admin){
-      localStorage.setItem("email",email)
-      localStorage.setItem("admin",admin)
-    }
-    function logout(){
-      setCurrentUser(null)
-      localStorage.clear()
-      window.location.reload()
-   }
-    function userget(){
-      setEmail(localStorage.getItem("email"))
-      setadmin(localStorage.getItem("admin"))
-    }
-  
-    useEffect(() => {
-        userget()
-        setCurrentUser(auth)
-    }, [])
+  useEffect(() => {
+    userget()
+    setCurrentUser(auth)
+  }, [])
 
 
-    const value = {
-      auth,
-      currentUser,
-      email,
-      admin,
-      loginState,
-      setAuth,
-      userSet,
-      SetloginState,
-      logout,
-    }
-    return (
-        <ServerContext.Provider value={value}>
-           {children}
-        </ServerContext.Provider>
-      )
+  const value = {
+    auth,
+    currentUser,
+    email,
+    admin,
+    loginState,
+    setAuth,
+    userSet,
+    SetloginState,
+    logout,
   }
+  return (
+    <ServerContext.Provider value={value}>
+      {children}
+    </ServerContext.Provider>
+  )
+}
